579f0052253883c23082dbeea64e1c1119ce5af8
V8tr <ne0_n@mail.ru>
2018-10-11 17:38:36 +0300

    Initial commit

---
+++ CollectionViewGridLayout-Starter/CollectionViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  CollectionViewGridLayout-Starter
+//
+//  Created by Vadim Bulavin on 10/1/18.
+//  Copyright © 2018 Vadim Bulavin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	@IBOutlet weak var gridCollectionView: UICollectionView!
+    
+    @IBOutlet weak var gridLayout: StickyGridCollectionViewLayout!
+}
+
+// MARK: - Collection view data source and delegate methods
+
+extension ViewController: UICollectionViewDataSource {
+
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 100
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.reuseID, for: indexPath) as? CollectionViewCell else {
+            return UICollectionViewCell()
+        }
+
+        cell.titleLabel.text = "\(indexPath)"
+
+        return cell
+    }
+}
+
+extension ViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: 100, height: 100)
+    }
+}
---
+++ CollectionViewGridLayout-Starter/StickyGridCollectionViewLayout.swift
@@ -0,0 +1,13 @@
+//
+//  GridCollectionViewLayout.swift
+//  CollectionViewGridLayout-Starter
+//
+//  Created by Vadim Bulavin on 10/1/18.
+//  Copyright © 2018 Vadim Bulavin. All rights reserved.
+//
+
+import UIKit
+
+class StickyGridCollectionViewLayout: UICollectionViewFlowLayout {
+
+}
